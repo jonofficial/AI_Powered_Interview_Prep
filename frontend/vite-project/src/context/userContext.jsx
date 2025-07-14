6dca6c58702735a8b319e8131e60eb462b5cbad6
jonofficial <jonrocky10@gmail.com>
2025-07-15 00:41:43 +0530

    created partial landingPage

---
+++ frontend/vite-project/src/context/userContext.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function userContext() {
+  return (
+    <div>userContext</div>
+  )
+}
+
+export default userContext
---
+++ frontend/vite-project/src/utils/data.js
@@ -0,0 +1,32 @@
+export const APP_FEATURES = [
+    {
+        id: "01",
+        title: "AI-Powered Mock Interviews",
+        description: "Experience realistic mock interviews with AI-generated questions tailored to your profile and job role.",
+    },
+    {
+        id: "02",
+        title: "Personalized Feedback",
+        description: "Receive detailed feedback on your performance, including strengths and areas for improvement.",
+    },
+    {
+        id: "03",
+        title: "Progress Tracking",
+        description: "Monitor your interview preparation progress with analytics and insights.",
+    },
+    {
+        id: "04",
+        title: "Resource Library",
+        description: "Access a library of resources, including articles, videos, and tips to enhance your interview skills.",
+    },
+    {
+        id: "05",
+        title: "Community Support",
+        description: "Join a community of job seekers and professionals to share experiences, tips, and support.",
+    },
+    {
+        id: "06",
+        title: "User-Friendly Interface",
+        description: "Navigate through a clean and intuitive interface designed for an optimal user experience.",
+    },
+];
