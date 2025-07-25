147ee54fb8db428e25a8646cfaf9b9925337058c
jonofficial <jonrocky10@gmail.com>
2025-07-26 01:47:05 +0530

    vercel analytics correction

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import {Toaster} from 'react-hot-toast';
-import { Analytics } from '@vercel/analytics/react';
 import Login from "./pages/Auth/Login";
 import SignUp from "./pages/Auth/SignUp";
 import LandingPage from "./pages/LandingPage";
@@ -31,7 +30,6 @@ const App = () => {
       }}
       />
     </div>
-     <Analytics />
     </UserProvider>
   ) 
 }
