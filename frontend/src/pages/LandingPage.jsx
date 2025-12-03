b1d22a8eb1197f43717ff9c92dce837c8d5aed60
S Jonathan Harrison <121378843+jonofficial@users.noreply.github.com>
2025-12-03 21:18:50 +0530

    small update

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -121,7 +121,7 @@ const LandingPage = () => {
     </div>
     </div>
     <div className='text-sm bg-gray-50 text-secondary text-center p-5 mt-5'>
-      Made by jonofficial
+      Made by Pranav
     </div>
     </div>
     <Modal
